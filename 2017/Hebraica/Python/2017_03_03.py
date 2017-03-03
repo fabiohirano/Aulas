25e4e67b5c40b9b1575cc675e80822f51fb31ea0
Fabio Hirano <fabiohirano@gmail.com>
2017-03-03 16:13:05 -0300

    Update 2017_03_03.py

--- 2017/Hebraica/Python/2017_03_03.py
+++ 2017/Hebraica/Python/2017_03_03.py
@@ -4,7 +4,7 @@ youtubers = ["pewdiepie", "jovemnerd", "rezendeevil", "felipeneto", "felipeneto 
 
 adjetivo = ["cabeçudo", "gordo", "pegador"]
 
-mortes = ["asfixiado com amoeba", "afogado em amoeba", "cagando", "escorregando na nutella", "tropeçando no Eevee", "atropelado por um rinoceronte bêbado", "caindo do penhasco, quebrando a perna e caindo de novo", "bêbado e drogado", "pisoteado por um dragao branco de olhos azuis e rabo cor de rosa", "dormindo", "levando um coco na cabeça", "splashado por uma magicarpa", "de avc", "ouvindo Justin Biba", "intoxicado por Dolly Guaraná", "jogando CS por 87 horas seguidas", "levando um soco de Jooooooooooohn Ceeeeeeeeeeenaaaaaaaaa", "correndo para chegar no banheiro", "decapitado por el disco vuelador", "flagrado pela mãe vendo pornografia"]
+mortes = ["asfixiado com amoeba", "afogado em amoeba", "escorregando na nutella", "tropeçando no Eevee", "atropelado por um rinoceronte bêbado", "caindo do penhasco, quebrando a perna e caindo de novo", "bêbado e drogado", "pisoteado por um dragao branco de olhos azuis e rabo cor de rosa", "dormindo", "levando um coco na cabeça", "splashado por uma magicarpa", "de avc", "ouvindo Justin Biba", "intoxicado por Dolly Guaraná", "jogando CS por 87 horas seguidas", "levando um soco de Jooooooooooohn Ceeeeeeeeeeenaaaaaaaaa", "correndo para chegar no banheiro", "decapitado por el disco vuelador", "flagrado pela mãe vendo pornografia"]
 
 tempo = random.randint(1, 60)
 
